787755112732f990f5544154e9f2447036ac0066
Paul Ogris <paul@tsahyt.com>
2016-10-21 17:28:32 +0200

    Add Iterative/Async solving modules

--- src/Clingo/Raw/Iterative.hs
+++ src/Clingo/Raw/Iterative.hs
@@ -1,8 +1,32 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
 module Clingo.Raw.Iterative
 (
+    solveIterativelyNext,
+    solveIterativelyGet,
+    solveIterativelyClose
 )
 where
 
+import Control.Monad.IO.Class
+import Clingo.Raw.Enums
+import Clingo.Raw.Types
+import Foreign
+
 foreign import ccall "clingo.h clingo_solve_iteratively_next" solveIterativelyNextFFI ::
+    Ptr IterSolver -> Ptr (Ptr Model) -> IO CBool
 foreign import ccall "clingo.h clingo_solve_iteratively_get" solveIterativelyGetFFI ::
+    Ptr IterSolver -> Ptr SolveResult -> IO CBool
 foreign import ccall "clingo.h clingo_solve_iteratively_close" solveIterativelyCloseFFI ::
+    Ptr IterSolver -> IO CBool
+
+solveIterativelyNext :: MonadIO m 
+                     => Ptr IterSolver -> Ptr (Ptr Model) -> m CBool
+solveIterativelyNext a b = liftIO $ solveIterativelyNextFFI a b
+
+solveIterativelyGet :: MonadIO m 
+                    => Ptr IterSolver -> Ptr SolveResult -> m CBool
+solveIterativelyGet a b = liftIO $ solveIterativelyGetFFI a b
+
+solveIterativelyClose :: MonadIO m 
+                      => Ptr IterSolver -> m CBool
+solveIterativelyClose a = liftIO $ solveIterativelyCloseFFI a
