7a662831cc4b86fb9c1f67a440ef012c67821814
Paul Ogris <paul@tsahyt.com>
2016-11-04 19:10:12 +0100

    Add Configuration example

---
+++ examples/Configuration.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Control.Monad
+import Clingo.Control
+import Clingo.Configuration
+import Clingo.Symbol
+import Clingo.Model
+import Data.StateVar
+
+import Text.Printf
+import qualified Data.Text.IO as T
+
+onModel :: Model s -> IO Continue
+onModel m = do
+    syms <- mapM prettySymbol
+        =<< modelSymbols m (selectNone { selectShown = True }) 
+    putStr "Model: " >> print syms
+    return Continue
+
+(>>?=) :: (Monad m, Foldable t) => m (t a) -> (a -> m b) -> m ()
+a >>?= b = a >>= mapM_ b
+    
+main :: IO ()
+main = withDefaultClingo $ \ctrl -> do
+    conf <- configuration ctrl
+
+    -- enumerate all models
+    fromConfig conf (atMap "solve" >=> atMap "models" >=> value) >>?= ($= "0")
+
+    -- use berkmin
+    fromConfig conf (atMap "solver" >=> atArray 0 >=> atMap "heuristic" 
+                      >=> value)
+        >>?= ($= "berkmin")
+    
+    addProgram ctrl "base" [] "a :- not b. b :- not a."
+    ground ctrl [Part "base" []] Nothing
+    void $ solve ctrl (Just onModel) []
