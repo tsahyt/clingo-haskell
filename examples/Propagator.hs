e0ca98ebf12662e2a348750f8f8b8b013f9bb072
Paul Ogris <paul@tsahyt.com>
2016-11-22 15:49:31 +0100

    Fix Pigeonator Bug
    While undoing, the hole would always be free'd instead of only when given the
    literal that actually occupied it. This meant that some conflicts were omitted.

--- examples/Propagator.hs
+++ examples/Propagator.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PatternGuards #-}
 module Main where
 
 import Control.Concurrent.MVar
@@ -22,8 +23,6 @@ import qualified Data.IntMap as I
 import Text.Printf
 import qualified Data.Text.IO as T
 
-import Debug.Trace
-
 newtype Hole = Hole Int
     deriving (Eq, Show, Ord)
 
@@ -38,10 +37,12 @@ assignHole tid hole lit pd = pd
     { assignments = 
         I.adjust (M.insert hole lit) (fromIntegral tid) (assignments pd) }
 
-unassignHole :: Integer -> Hole -> PigeonData s -> PigeonData s
-unassignHole tid hole pd = pd
-    { assignments =
-        I.adjust (M.delete hole) (fromIntegral tid) (assignments pd) }
+unassignHole :: Integer -> Literal s -> Hole -> PigeonData s -> PigeonData s
+unassignHole tid lit hole pd = pd
+    { assignments = I.adjust go (fromIntegral tid) (assignments pd) }
+    where go m | Just l <- M.lookup hole m
+               , l == lit  = M.delete hole m
+               | otherwise = m
 
 forceMVar :: MonadIO m => MVar a -> a -> m ()
 forceMVar mvar x = liftIO $ do
@@ -93,8 +94,6 @@ pigeonator ctrl mvar = emptyPropagator
             let xs = zip (take threads [0..]) (repeat M.empty)
              in PigeonData (M.fromList . catMaybes $ ps) (I.fromList xs)
 
-        liftIO (withMVar mvar print)
-
     , propPropagate = Just $ \changes -> do
         -- get previous assignment
         thread <- getThreadId
@@ -106,18 +105,21 @@ pigeonator ctrl mvar = emptyPropagator
             let Just hole = lit `M.lookup` placements state
                 prev = hole `M.lookup` holes
             case prev of
-                Nothing -> liftIO $ 
+                Nothing -> liftIO $
                     modifyMVar_ mvar (return . assignHole thread hole lit)
                 Just p -> do
                     let c = Clause (map negateLiteral [lit, p]) ClauseLearnt
-                    liftIO (print c)
                     addClause c
                     propagate
 
     , propUndo = Just $ \changes -> do
         thread <- getThreadId
         state  <- liftIO (readMVar mvar)
-        let cs' = mapMaybe (`M.lookup` placements state) changes
+
+        let cs' = mapMaybe 
+                      (\l -> (,) <$> pure l <*> M.lookup l (placements state))
+                      changes
         liftIO $ 
-            modifyMVar_ mvar (return . flip (foldr (unassignHole thread)) cs')
+            modifyMVar_ mvar $
+            return . flip (foldr (uncurry (unassignHole thread))) cs'
     }
