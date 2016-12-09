c61908f8e6490d14e2ac04ef288a566b6f66624c
Paul Ogris <paul@tsahyt.com>
2016-12-09 11:09:06 +0100

    Add Clingo main module
    This provides a more convenient way to import the most important
    functionality. Not everything is being exported here.

---
+++ src/Clingo.hs
@@ -0,0 +1,12 @@
+module Clingo
+(
+    module X
+)
+where
+
+import Clingo.Control as X
+import Clingo.Model as X
+import Clingo.Symbol as X
+import Clingo.Propagation as X
+import Clingo.Statistics as X
+import Clingo.Configuration as X
